1362f8ed19773b73d611cdc2d00bef8d63948bfd
Teresa <teresacing771995@gmail.com>
2019-05-24 16:19:51 +0630

    customize liveGamae

--- src/components/LiveGame.vue
+++ src/components/LiveGame.vue
@@ -6,7 +6,7 @@
       </v-flex>
     </v-layout>
 
-    <v-layout row class="pt-1" style="height:100vh">
+    <v-layout row class="pt-1" style="height:100%">
       <v-flex xs3>
         <div
           v-for="i in livegames"
@@ -17,209 +17,69 @@
           <span @click="current = i.title">{{i.title}}</span>
         </div>
       </v-flex>
+
       <v-flex xs9>
         <!-- one -->
         <v-layout row wrap v-if="current=='电子'">
-          <v-layout>
+          <v-layout v-for="item in electronic" :key="item.gameName">
             <v-flex xs6>
-              <img src="@/assets/1.png" height="80" width="120" class="pl-3">
+              <img :src="item.image" height="80" width="120">
             </v-flex>
             <v-flex xs6>
-              <span class="ml-3 white--text">SW电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/1.png" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">PT电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/1.png" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">MG电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/1.png" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">HABA电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
+              <span class="white--text pl-3">{{item.gameName}}</span>
+              <v-btn round small color="red darken-4 mt-4" dark>开始游戏</v-btn>
             </v-flex>
           </v-layout>
         </v-layout>
-
         <!-- two -->
         <v-layout row wrap v-if="current=='棋牌'">
-          <v-layout>
+          <v-layout v-for="item in chess" :key="item.gameName">
             <v-flex xs6>
-              <img src="@/assets/2.jpg" height="80" width="120" class="pl-3">
+              <img :src="item.image" height="80" width="120">
             </v-flex>
             <v-flex xs6>
-              <span class="ml-3 white--text">SW电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/2.jpg" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">PT电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/2.jpg" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">MG电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/2.jpg" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">HABA电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
+              <span class="white--text pl-3">{{item.gameName}}</span>
+              <v-btn round small color="red darken-4 mt-4" dark>开始游戏</v-btn>
             </v-flex>
           </v-layout>
         </v-layout>
-
         <!-- three -->
-        <v-layout row wrap v-if="current=='真人'">
-          <v-layout>
+           <v-layout row wrap v-if="current=='真人'">
+          <v-layout v-for="item in casino" :key="item.gameName">
             <v-flex xs6>
-              <img src="@/assets/3.jpg" height="80" width="120" class="pl-3">
+              <img :src="item.image" height="80" width="120">
             </v-flex>
             <v-flex xs6>
-              <span class="ml-3 white--text">SW电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/3.jpg" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">PT电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/3.jpg" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">MG电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/3.jpg" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">HABA电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
+              <span class="white--text pl-3">{{item.gameName}}</span>
+              <v-btn round small color="red darken-4 mt-4" dark>开始游戏</v-btn>
             </v-flex>
           </v-layout>
         </v-layout>
         <!-- four -->
-        <v-layout row wrap v-if="current=='电竞'">
-          <v-layout>
+           <v-layout row wrap v-if="current=='电竞'">
+          <v-layout v-for="item in game" :key="item.gameName">
             <v-flex xs6>
-              <img src="@/assets/4.jpg" height="80" width="120" class="pl-3">
+              <img :src="item.image" height="80" width="120">
             </v-flex>
             <v-flex xs6>
-              <span class="ml-3 white--text">SW电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/download.jpg" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">PT电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/4.jpg" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">MG电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
-            </v-flex>
-          </v-layout>
-
-          <v-layout>
-            <v-flex xs6>
-              <img src="@/assets/4.jpg" height="80" width="120" class="pl-3">
-            </v-flex>
-            <v-flex xs6>
-              <span class="ml-3 white--text">HABA电子</span>
-              <div class="text-xs-center">
-                <v-btn round small color="red darken-4" dark class="mr-5 mt-4">开始游戏</v-btn>
-              </div>
+              <span class="white--text pl-3">{{item.gameName}}</span>
+              <v-btn round small color="red darken-4 mt-4" dark>开始游戏</v-btn>
             </v-flex>
           </v-layout>
         </v-layout>
+        <!-- five -->
+           <v-layout row wrap v-if="current=='体育'">
+          <v-layout v-for="item in sport" :key="item.gameName">
+            <v-flex xs6>
+              <img :src="item.image" height="80" width="120">
+            </v-flex>
+            <v-flex xs6>
+              <span class="white--text pl-3">{{item.gameName}}</span>
+              <v-btn round small color="red darken-4 mt-4" dark>开始游戏</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-layout>
+
       </v-flex>
     </v-layout>
   </div>
@@ -243,7 +103,36 @@ export default {
         { title: "关于" }
       ],
       current: "电子",
-      // 真人: true
+      electronic: [
+        { image: require("../assets/3.jpg"), gameName: "HABA电子" },
+        { image: require("../assets/3.jpg"), gameName: "MG电子" },
+        { image: require("../assets/3.jpg"), gameName: "PT电子" },
+        { image: require("../assets/3.jpg"), gameName: "SW电子" }
+      ],
+      chess: [
+        { image: require("../assets/4.jpg"), gameName: "HABA电子" },
+        { image: require("../assets/4.jpg"), gameName: "MG电子" },
+        { image: require("../assets/4.jpg"), gameName: "PT电子" },
+        { image: require("../assets/4.jpg"), gameName: "SW电子" }
+      ],
+       casino: [
+        { image: require("../assets/5.jpg"), gameName: "HABA电子" },
+        { image: require("../assets/5.jpg"), gameName: "MG电子" },
+        { image: require("../assets/5.jpg"), gameName: "PT电子" },
+        { image: require("../assets/5.jpg"), gameName: "SW电子" }
+      ],
+       game: [
+        { image: require("../assets/2.jpg"), gameName: "HABA电子" },
+        { image: require("../assets/2.jpg"), gameName: "MG电子" },
+        { image: require("../assets/2.jpg"), gameName: "PT电子" },
+        { image: require("../assets/2.jpg"), gameName: "SW电子" }
+      ],
+      sport: [
+        { image: require("../assets/1.png"), gameName: "HABA电子" },
+        { image: require("../assets/1.png"), gameName: "MG电子" },
+        { image: require("../assets/1.png"), gameName: "PT电子" },
+        { image: require("../assets/1.png"), gameName: "SW电子" }
+      ]
     };
   },
   methods: {}
@@ -256,8 +145,8 @@ export default {
 .margin {
   padding-top: 10px;
 }
-.menucolor{
-  background-color:#FF8F00
+.menucolor {
+  background-color: #ff8f00;
 }
 </style>
 
